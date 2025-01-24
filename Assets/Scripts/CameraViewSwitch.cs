74dbca690023aa1fcb41118cb097656fd97b6882
Srixx24 <jacolynlovins@gmail.com>
2025-01-24 01:25:25 -0600

    Preparing script structure and organization hierarchy

--- Assets/Scripts/CameraViewSwitch.cs
+++ Assets/Scripts/CameraViewSwitch.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WorldObjectsManager : MonoBehaviour
+public class CameraViewSwitch : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
