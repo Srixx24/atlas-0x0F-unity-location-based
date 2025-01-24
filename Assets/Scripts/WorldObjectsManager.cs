944246791126d2a224961fc466339967a57d49e3
Srixx24 <jacolynlovins@gmail.com>
2025-01-24 00:29:15 -0600

    Adding new packages and updating lightship

--- Assets/Scripts/WorldObjectsManager.cs
+++ Assets/Scripts/WorldObjectsManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WorldLocations : MonoBehaviour
+public class WorldObjectsManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
